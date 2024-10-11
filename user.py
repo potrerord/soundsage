035e90e910afab0a1bac1dae24e65fe8f98b9761
potrerord <125526503+potrerord@users.noreply.github.com>
2024-10-10 22:38:23 -0400

    Remove notes from initial file docstring.

--- user.py
+++ user.py
@@ -1,36 +1,5 @@
 """
 @brief A synthetic User to provide feedback to SoundSage algorithm.
-
-High Level Detail:
-- User Model
-    - Id
-    - Song_listened/song history
-        - Lists of songs that the user has listened to
-    - Favorite_genre
-        - Lists of genres that this user likes
-        - “Feedback” for the algorithm to know whether it’s predicting things right
-- Overview of User design
-    - Think of a user as a bucket where we store song data. Each user should have different preferences that we assign, therefore as the algorithm predicts the preference of this bucket of song data, it should predict the preference we programmed it to. 
-- Model after Spotify Recommendation format
-    - https://developer.spotify.com/documentation/web-api/reference/get-recommendations
-    - Have target acousticness, danceability, and set a min/max range to recommend songs similar to that genre/artists. 
-    - Will be using the track data as our source information in song history.
-    - https://developer.spotify.com/documentation/web-api/reference/get-track 
-- Output data into a file that can be parsed by algorithm
-    - .csv would be common
-- Spotify Recommendation Model
-    - https://developer.spotify.com/documentation/web-api/reference/get-recommendations
- 
-Data Generation
-- User: 
-    - favorite_genre: choose from a list of genres. Set users preference to said genre. 
-    - Way to generate music
-        - Use GetRecommendation from Spotify API to generate seed data based on artist and genre
-        - Example: 60% are from specific recommended list, 40% are random
-        - The Algorithm should be able to recommend similar songs in our favorite_genre list that we assigned for this user. 
-- Model/Algorithm
-    - Agent’s job is to determine what recommendations we decided on, figure out the genre, which should match the recommendation seed parameters we created in the last step. 
-    - Then it should be able to make recommendations based on min-max danceability, etc.
 """
 
 
@@ -48,7 +17,7 @@ class User:
 
     _liked_genres: list[str]
     """List of genres that this user likes."""
-    
+
     # TODO: Add more preferences (acousticness(?), etc.)
 
     def __init__(
@@ -71,7 +40,7 @@ class User:
         """
 
         # TODO: Process song list according to user preferences.
-        
+
         # TODO: Condense into a float in range [0, 1] ?
         ...
 
@@ -117,7 +86,7 @@ class User:
         """
 
         self._liked_songs = liked_songs
-        
+
     def set_liked_genres(
             self: "User",
             liked_genres: list[str]
@@ -126,5 +95,5 @@ class User:
         
         :param liked_genres: the user's liked genres
         """
-        
+
         self._liked_genres = liked_genres
