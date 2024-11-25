78543ae944c5ff840c9a094a348d54b9aa751c35
potrerord <125526503+potrerord@users.noreply.github.com>
2024-11-25 16:58:34 -0500

    Add [str(song) for song in recommended_songs] so that the printed lists use the readable __str__() method instead of the debugging __repr__ method.

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def main() -> None:
     print("\nGetting recommended songs...")
     recommended_songs: list[Song] = recommender_aggregator.recommend()
 
-    print("\nRecommended songs:\n{*recommended_songs}", sep='\n')
+    print(f"\nRecommended songs:\n{[str(song) for song in recommended_songs]}")
 
     print("\nProviding mock feedback...")
     like_dislike_feedback_strategy.execute(user_id=DEFAULT_USER_ID, song=recommended_songs[0], liked=True)
